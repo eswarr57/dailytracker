936a6882d739b5f3560b0675c54d81c9f9ea1758
eswarr57 <kaligatlaeswarr@gmail.com>
2026-05-16 22:39:02 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ connectDB();
 /*
   MIDDLEWARE
 */
-app.use(cors());
+app.use(cors({
+  origin: "https://dailytracker-omega.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- frontend/js/analytics.js
+++ frontend/js/analytics.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://dailytracker-611b.onrender.com/api";
 const token = localStorage.getItem("token");
 
 if (!token) {
